404326cc5bf000f8ecd14d0d4179f5856460dc6a
aqShanto <abdulquymshanto@gmail.com>
2024-11-04 14:34:51 +0600

    up

--- Long contest/Binary Search 1.2/f.cpp
+++ Long contest/Binary Search 1.2/f.cpp
@@ -1,59 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define int long long
 #define endl "\n"
-double pi= 3.141592653589793238462643383279;
-/*
-7 13
-6 8 14 9 4 11 10
-*/
+double pi = 3.141592653589793238462643383279;
 
-void solve(){
-    int n,t;
-    cin>>n>>t;
-    int ar[n];
-    for(int i=0;i<n;i++){
-        cin>>ar[i];
+void solve()
+{
+    int n, t;
+    cin >> n >> t;
+    int ar[n], prefix[n + 1] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
     }
-
-    vector<int>pre(n,0);
-    pre[0]=ar[n-1];
-    int j=1;
-    for(int i=n-2;i>=0;--i){
-        pre[j]=pre[j-1]+ar[i];
+    int j = 1;
+    for (int i = n - 1; i >= 0; --i)
+    {
+        prefix[j] = prefix[j - 1] + ar[i];
         j++;
     }
-
-    for(int i=0;i<n;i++){
-        cout<<pre[i]<<" ";
-    }
-    cout<<endl;
-
-    int lo=0,hi=n-1,mid,ans=-1;
-    while(lo<=hi){
-        mid=(lo+hi)/2;
-        if(pre[mid]==t){
-            ans=mid;
-            break;
-        }else if(pre[mid]<t){
-            ans=mid;
-            lo=mid+1;
+    int cnt = INT_MIN;
+    for (int i = 1; i <= n; i++)
+    {
+        int lo = i, hi = n, mid, ans = -1;
+        while (lo <= hi)
+        {
+            mid = (lo + hi) / 2;
+            if (prefix[mid] - prefix[i - 1] <= t)
+            {
+                lo = mid + 1;
+            }
+            else if (prefix[mid] - prefix[i - 1] > t)
+            {
+                hi = mid - 1;
+            }
         }
-        else if(pre[mid]>t){
-            hi=mid-1;
-        }
+        cnt = max(cnt, lo - i);
     }
-
-    ans++;
-    cout<<ans<<endl;
+    cout << cnt << endl;
 }
 
-signed main(){
+signed main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int t=1;
+    int t = 1;
     // cin>>t;
-    while(t--){
+    while (t--)
+    {
         solve();
     }
     return 0;
--- Long contest/DIU Advance Camp Searching and sorting/d.cpp
+++ Long contest/DIU Advance Camp Searching and sorting/d.cpp
@@ -1,27 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define int long long
 #define endl "\n"
-double pi= 3.141592653589793238462643383279;
+double pi = 3.141592653589793238462643383279;
 
-void solve(){
-    int n,k;
-    cin>>n>>k;
-    if(k<n){
-        cout<<k<<endl;
-    }else if(k%n!=0 && k>n){
-        cout<<k+(k/n)+1<<endl;
-    }else{
-        cout<<k+(k/n)<<endl;
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    int lo = 1, hi = 2e9, mid;
+    while (lo <= hi)
+    {
+        mid = (lo + hi) / 2;
+        int ele = mid / k;
+        int val = (mid * (mid + 1)) / 2;
     }
 }
 
-signed main(){
+signed main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int t=1;
-    cin>>t;
-    while(t--){
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
         solve();
     }
     return 0;
